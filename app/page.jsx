28202114a4befb74a12298d34197f77620e86e06
Kenneth Cayetano <80654992+IKENN007@users.noreply.github.com>
2024-01-30 18:01:23 +0800

    Initial start of About Page

--- app/page.jsx
+++ app/page.jsx
@@ -5,8 +5,20 @@ export default function Home() {
     <>
       <Hero />
 
-      <div className="md:px-20">
-        About Me
+      <div className="px-7 pt-28 bg-slate-900 text-white flex
+      md:px-20">
+
+        <div className="w-2/4 border"></div>
+
+        <div className="w-2/4 border">
+
+          <h2 className="mb-6 text-5xl font-bold">About Me</h2>
+
+          <p className=" mb-4 text-base">
+            I am an enthusiastic web developer living in General Trias, Cavite,
+            with 3 years of experience in web application, front-end development, and REST API.
+          </p>
+        </div>
       </div>
     </>
   );
