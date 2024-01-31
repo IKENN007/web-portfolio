a9ad042da626ea86ae60af7199e5c84b835ac27a
Kenneth Cayetano <80654992+IKENN007@users.noreply.github.com>
2024-01-31 09:16:03 +0800

    Separate the About Section, created a About component, and import to page

---
+++ app/components/landing-components/About.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <>
+            <div className="px-7 pt-28 bg-slate-900 text-white flex
+            md:px-20">
+
+                <div className="w-2/5  hidden md:block"></div>
+
+                <div className="w-full md:w-3/5 pb-12">
+
+                    <div className="pb-4">
+                        <h2 className="mb-6 text-5xl font-bold">About Me</h2>
+
+                        <div className="mb-4 px-2">
+                            <p className="text-base">
+                                I am an enthusiastic web developer living in General Trias, Cavite,
+                                with 3 years of experience in web application, front-end development, and REST API.
+                            </p>
+                        </div>
+
+                        <ul id="details-container" className="px-2 mt-6">
+                        <li className="md:flex mb-3">
+                            <span className="w-1/4 font-semibold text-base me-2">Name: </span>
+                            <span className="w-3/4 text-base">Jon Kenneth C. Alderite</span>
+                        </li>
+                        <li className="md:flex mb-3">
+                            <span className="w-1/4 font-semibold text-base me-2">Date of birth: </span>
+                            <span className="w-3/4 text-base">July 09, 1999</span>
+                        </li>
+                        <li className="md:flex mb-3">
+                            <span className="w-1/4 font-semibold text-base me-2">Phone: </span>
+                            <span className="w-3/4 text-base">+63 928 574 7667</span>
+                        </li>
+                        <li className="md:flex mb-3">
+                            <span className="w-1/4 font-semibold text-base me-2">Email: </span>
+                            <span className="w-3/4 text-base">Jonkennethcayetano@gmail.com</span>
+                        </li>
+                        
+                        </ul>
+                    </div>
+
+                    <div className=" mt-4">
+                        <button className="bg-customYellow uppercase text-secondaryColor px-6 py-4 
+                        rounded-2xl text-xs font-semibold mb-4">
+                        Download CV
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default About
--- app/page.jsx
+++ app/page.jsx
@@ -1,25 +1,12 @@
 import Hero from "./components/landing-components/Hero";
+import About from "./components/landing-components/About";
 
 export default function Home() {
   return (
     <>
       <Hero />
-
-      <div className="px-7 pt-28 bg-slate-900 text-white flex
-      md:px-20">
-
-        <div className="w-2/4 border"></div>
-
-        <div className="w-2/4 border">
-
-          <h2 className="mb-6 text-5xl font-bold">About Me</h2>
-
-          <p className=" mb-4 text-base">
-            I am an enthusiastic web developer living in General Trias, Cavite,
-            with 3 years of experience in web application, front-end development, and REST API.
-          </p>
-        </div>
-      </div>
+      <About />
+      
     </>
   );
 }
