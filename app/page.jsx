27e859da5c37d9aa9b9808208c64475d73375a85
Kenneth Cayetano <80654992+IKENN007@users.noreply.github.com>
2024-01-31 17:19:06 +0800

    Imported the Skill and Experience component.

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,16 @@
 import Hero from "./components/landing-components/Hero";
 import About from "./components/landing-components/About";
+import Skill from "./components/landing-components/Skill";
+import Experience from "./components/landing-components/Experience";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
-      
+      <Skill />
+      <div className="h-96 bg-prog-bg bg-cover bg-fixed bg-right md:bg-center"></div>
+      <Experience />
     </>
   );
 }
