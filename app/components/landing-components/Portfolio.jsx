86ffe65fd1211d286061e21bb00a6d4d905dbfa6
Kenneth Cayetano <80654992+IKENN007@users.noreply.github.com>
2024-05-03 16:30:04 +0800

    Update Portfolio.jsx

--- app/components/landing-components/Portfolio.jsx
+++ app/components/landing-components/Portfolio.jsx
@@ -1,201 +1,198 @@
 import Image from "next/image"
 
+import { 
+    FaHtml5,
+    FaCss3,
+    FaJs,
+    FaPhp,
+    FaPython,
+    FaBootstrap
+} from "react-icons/fa6";
+
+import { 
+    SiJquery,
+    SiTailwindcss,
+    SiNextdotjs,
+    SiNodedotjs,
+    SiReact,
+    SiLaravel,
+    SiMicrosoftsqlserver
+} from "react-icons/si";
+
+
+const systemProjects = [
+    {
+        id: '0',
+        imageSrc: '/images/prpo_system_img/prpo-system-landing-page.png',
+        Name: 'PRPO',
+        OtherTerm: 'Purchasing Department System',
+        Description: '',
+        ProgLanguages: [
+            {pl: 'Html5'},
+            {pl: 'CSS'},
+            {pl: 'Javascript'},
+            {pl: 'JQuery'},
+            {pl: 'Php'},
+            {pl: 'Python'},
+            {pl: 'Bootstrap'},
+            {pl: 'Microsoft SQL Server'},
+        ]
+    },
+    {
+        id: '1',
+        imageSrc: '/images/ppic_system_img/ppic-system-landing-page.png',
+        Name: 'PPIC PO Monitoring',
+        OtherTerm: 'Manufacturing / Production Parts Inventory Control Department System',
+        Description: '',
+        ProgLanguages: [
+            {pl: 'Html5'},
+            {pl: 'CSS'},
+            {pl: 'Javascript'},
+            {pl: 'React JS'},
+            {pl: 'Next JS'},
+            {pl: 'Node JS'},
+            {pl: 'Bootstrap'},
+            {pl: 'Php'},
+            {pl: 'Laravel'},
+            {pl: 'Microsoft SQL Server'},
+        ]
+    },
+    {
+        id: '2',
+        imageSrc: '/images/receiving_img/material-receiving-page.png',
+        Name: 'Materials Receiving System',
+        OtherTerm: 'Warehouse Department System',
+        Description: '',
+        ProgLanguages: [
+            {pl: 'Html5'},
+            {pl: 'CSS'},
+            {pl: 'Javascript'},
+            {pl: 'JQuery'},
+            {pl: 'Microsoft SQL Server'},
+        ]
+    },
+    {
+        id: '3',
+        imageSrc: '/images/facilities_system_img/facilities-pm-schedule.png',
+        Name: 'Facilities Admin Dashboard',
+        OtherTerm: 'Facilities Department System',
+        Description: '',
+        ProgLanguages: [
+            {pl: 'Html5'},
+            {pl: 'CSS'},
+            {pl: 'Javascript'},
+            {pl: 'JQuery'},
+            {pl: 'Microsoft SQL Server'},
+        ]
+    },
+]
+
 const Portfolio = () => {
+
+    const getProgLangIcon = (progValue) => {
+
+        let progLangIcon
+
+        switch (progValue) {
+            case 'Html5':
+                progLangIcon = <FaHtml5 fontSize={16} fill="#FFBD39" />;
+                break;
+            case 'CSS':
+                progLangIcon = <FaCss3 fontSize={16} fill="#FFBD39" />;
+                break;
+            case 'Javascript':
+                progLangIcon = <FaJs fontSize={16} fill="#FFBD39" />;
+                break;
+            case 'Php':
+                progLangIcon = <FaPhp fontSize={16} fill="#FFBD39" />;
+                break;
+            case 'Python':
+                progLangIcon = <FaPython fontSize={16} fill="#FFBD39" />;
+                break;
+            case 'JQuery':
+                progLangIcon = <SiJquery fontSize={16} fill="#FFBD39" />;
+                break;
+            case 'React JS':
+                progLangIcon = <SiReact fontSize={16} fill="#FFBD39" />;
+                break;
+            case 'Next JS':
+                progLangIcon = <SiNextdotjs fontSize={16} fill="#FFBD39" />;
+                break;
+            case 'Node JS':
+                progLangIcon = <SiNodedotjs fontSize={16} fill="#FFBD39" />;
+                break;
+            case 'Laravel':
+                progLangIcon = <SiLaravel fontSize={16} fill="#FFBD39" />;
+                break;
+            case 'Microsoft SQL Server':
+                progLangIcon = <SiMicrosoftsqlserver fontSize={16} fill="#FFBD39" />;
+                break;
+        }
+
+        return progLangIcon;
+    }
+
     return (
-        <div className="px-7 pt-28 bg-slate-900 text-white md:px-20">
+        <>
+            <div className="px-7 pt-28 bg-slate-900 text-white md:px-20">
 
-            <div className="w-full pb-12">
-            <h2 id="projects" 
-            className="text-4xl md:text-5xl font-bold mb-6 text-center">Projects</h2>
-            </div>
-
-            <div id="project-image-container">
-
-                <div className="md:flex">
-                    <div className="md:w-1/2 lg:w-2/3">
-                        <div className="relative">
-                            <div className="absolute flex z-10 opacity-0 hover:opacity-100
-                            bg-secondaryColor bg-opacity-60 h-full w-full rounded-lg scale-95
-                            items-center justify-center duration-300 cursor-pointer">
-                                <div>
-                                    <div className="w-full">
-                                        <h4 className="text-customYellow w-full font-semibold uppercase text-xl 
-                                        lg:text-3xl">
-                                            PRPO System Landing Page
-                                        </h4>
-                                    </div>
-
-                                    <div className="w-full">
-                                    <span className="text-customYellow text-xs font-semibold uppercase">
-                                        Purchasing Department System
-                                    </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <Image src='/images/prpo_system_img/prpo-system-landing-page.png'
-                            loading="lazy" 
-                            alt="project-1"
-                            width={1000}
-                            height={1000}
-                            className="rounded-lg mb-2 cursor-pointer scale-95 md:scale-95
-                            hover:scale-100 duration-300 w-full h-auto"
-                            />
-                        </div>
-                    </div>
-
-                    <div className="md:w-1/2 lg:w-1/3 h-max">
-                        <div className="relative">
-                            <div className="absolute flex z-10 opacity-0 hover:opacity-100
-                            bg-secondaryColor bg-opacity-60 h-full w-full rounded-lg scale-95 lg:scale-90
-                            items-center justify-center duration-300 cursor-pointer">
-                                <div>
-                                    <div className="w-full">
-                                        <h4 className="text-customYellow w-full font-semibold uppercase text-xl">
-                                            PRPO System Items Page
-                                        </h4>
-                                    </div>
-                                    <div className="w-full">
-                                        <span className="text-customYellow text-xs font-semibold uppercase">
-                                            Add items to the cart to request purchasing.
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <Image src='/images/prpo_system_img/prpo-matcode-list.png'
-                            loading="lazy" 
-                            alt="project-1"
-                            width={1000}
-                            height={1000}
-                            className="rounded-lg mb-2 md:mb-0 cursor-pointer scale-95 lg:scale-90
-                            hover:scale-100 duration-300 w-full h-auto"
-                            />
-                        </div>
-
-                        <div className="relative hidden lg:block">
-                            <div className="absolute flex z-10 opacity-0 hover:opacity-100
-                            bg-secondaryColor bg-opacity-60 h-full w-full rounded-lg scale-95 lg:scale-90
-                            items-center justify-center duration-300 cursor-pointer">
-                                <div>
-                                    <div className="w-full">
-                                        <h4 className="text-customYellow w-full font-semibold uppercase text-xl">
-                                            PRPO System Request Details Page
-                                        </h4>
-                                    </div>
-                                    <div className="w-full">
-                                        <span className="text-customYellow text-xs font-semibold uppercase">
-                                            Monitor the approval status and delivery.
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <Image src='/images/prpo_system_img/prpo-request-details.png'
-                            loading="lazy" 
-                            alt="project-1"
-                            width={1000}
-                            height={1000}
-                            className="rounded-lg mb-2 md:mb-0 cursor-pointer scale-95 lg:scale-90
-                            hover:scale-100 duration-300 w-full h-auto"
-                            />
-                        </div>
-                    </div>
+                <div className="w-full pb-12">
+                    <h2 id="projects" 
+                    className="text-4xl md:text-5xl font-bold mb-6 text-center">
+                        Projects
+                    </h2>
                 </div>
 
-                <div className="lg:hidden flex justify-center">
-                    <div className="md:w-1/2">
-                        <div className="relative">
-                            <div className="absolute flex z-10 opacity-0 hover:opacity-100
-                            bg-secondaryColor bg-opacity-60 h-full w-full rounded-lg scale-95 lg:scale-90
-                            items-center justify-center duration-300 cursor-pointer">
-                                <div>
-                                    <div className="w-full">
-                                        <h4 className="text-customYellow w-full font-semibold uppercase text-xl">
-                                            PRPO System Request 
-                                            <br className="sm:hidden md:block"></br> 
-                                            Details Page
-                                        </h4>
-                                    </div>
-                                    <div className="w-full">
-                                        <span className="text-customYellow text-xs font-semibold uppercase">
-                                            Monitor the approval status and delivery.
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                            <Image src='/images/prpo_system_img/prpo-request-details.png'
-                            loading="lazy" 
-                            alt="project-1"
-                            width={1000}
-                            height={1000}
-                            className="rounded-lg mb-2 cursor-pointer scale-95 lg:scale-90
-                            hover:scale-100 duration-300 w-full h-auto"
-                            />
-                        </div>
-                    </div>
-                </div>
+                <div className="flex justify-center flex-wrap gap-8">
+                    {/* shadow-xl w-3/4 */}
 
-                <div className="mb-4 md:flex">
-                    <div className="md:w-1/2">
-                        <div className="relative">
-                            <div className="absolute flex z-10 opacity-0 hover:opacity-100
-                            bg-secondaryColor bg-opacity-60 h-full w-full rounded-lg scale-95
-                            items-center justify-center duration-300 cursor-pointer">
-                                <div>
-                                    <div className="w-full">
-                                        <h4 className="text-customYellow w-full font-semibold uppercase text-xl">
-                                            PPIC System Landing Page
-                                        </h4>
-                                    </div>
-                                    <div className="w-full">
-                                        <span className="text-customYellow text-xs font-semibold uppercase">
-                                            Manufacturing Department System
-                                        </span>
-                                    </div>
-                                </div>
+                    {systemProjects.map((proj) => (
+                        <div key={proj.id} className="bg-base-100 w-5/6 rounded-lg bg-secondaryColor grid grid-cols-12">
+                            <div className="col-span-7 rounded-s-lg">
+                                <Image src={`${proj.imageSrc}`}
+                                    loading="lazy" 
+                                    alt="project-1"
+                                    width={1000}
+                                    height={1000}
+                                    className="rounded-s-lg w-full h-auto"
+                                />
                             </div>
-                            <Image src='/images/ppic_system_img/ppic-system-landing.png'
-                            loading="lazy" 
-                            alt="project-1"
-                            width={1000}
-                            height={1000}
-                            className="rounded-lg mb-2 cursor-pointer scale-95
-                            hover:scale-100 duration-300 w-full h-auto"
-                            />
-                        </div>
-                    </div>
 
-                    <div className="md:w-1/2">
-                        <div className="relative">
-                            <div className="absolute flex z-10 opacity-0 hover:opacity-100
-                            bg-secondaryColor bg-opacity-60 h-full w-full rounded-lg scale-95
-                            items-center justify-center duration-300 cursor-pointer">
+                            <div className="col-span-5 p-7 relative flex flex-col justify-between">
                                 <div>
-                                    <div className="w-full">
-                                        <h4 className="text-customYellow w-full font-semibold uppercase text-xl">
-                                            Facilities Admin Dashboard
-                                        </h4>
-                                    </div>
-                                    <div className="w-full">
-                                        <span className="text-customYellow text-xs font-semibold uppercase">
-                                            Facilities Department System
+                                    <div className="mb-3">
+                                        <span className="font-black text-2xl text-customYellow">
+                                            {proj.Name}
                                         </span>
+                                        <p className="text-base">
+                                            {proj.OtherTerm}
+                                        </p>
+                                    </div>
+
+                                    <div className="w-full flex flex-wrap gap-2">
+                                        {proj.ProgLanguages.map((progLang) => (
+                                            <div key={progLang.pl} 
+                                                className="px-2 py-1 rounded-lg ring-1 ring-customYellow w-fit flex items-center gap-1">
+                                                {/* <FaHtml5 fontSize={16} fill="#FFBD39" /> */}
+                                                {getProgLangIcon(progLang.pl)}
+                                                <span className="text-xs text-customYellow">
+                                                    {progLang.pl}
+                                                </span>
+                                            </div>
+                                        ))}
                                     </div>
                                 </div>
+
+                                <div className="flex justify-end">
+                                    <button className="bg-customYellow uppercase text-secondaryColor px-6 py-4 rounded-lg text-xs font-semibold outline-none">View</button>
+                                </div>
                             </div>
-                            <Image src='/images/facilities_system_img/facilities-pm-schedule.png'
-                            loading="lazy" 
-                            alt="project-1"
-                            width={1000}
-                            height={1000}
-                            className="rounded-lg mb-2 cursor-pointer scale-95
-                            hover:scale-100 duration-300 w-full h-auto"
-                            />
                         </div>
-                    </div>
+                    ))}
                 </div>
 
             </div>
-
-        </div>
+        </>
     )
 }
 
